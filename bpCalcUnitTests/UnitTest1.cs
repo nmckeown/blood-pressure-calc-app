203ffe5d416d9dd13865090d39367681c3f1cfd2
Noel McKeown <nmckeown@salesforce.com>
2024-11-08 11:51:55 +0000

    add unit tests

--- bpCalcUnitTests/UnitTest1.cs
+++ bpCalcUnitTests/UnitTest1.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BPCalculator;
 
-namespace BPCalculatorUnitTest
+namespace BPCalculatorUnitTests
 {
     [TestClass]
     public class UnitTest1
