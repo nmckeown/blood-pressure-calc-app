4632d70030fb6858db7478fbe7247f08bf7ce4f9
Noel McKeown <nmckeown@salesforce.com>
2024-11-11 21:01:20 +0000

    test: updated unit

--- bpCalcUnitTests/UnitTest1.cs
+++ bpCalcUnitTests/UnitTest1.cs
@@ -6,74 +6,101 @@ namespace BPCalculatorUnitTests
     [TestClass]
     public class UnitTest1
     {
+        private BloodPressure bp;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initialize the BloodPressure instance before each test
+            bp = new BloodPressure();
+        }
+
         [TestMethod]
         public void TestLowBloodPressure()
         {
-            BloodPressure bp = new BloodPressure() { Systolic = 80, Diastolic = 50 };
+            bp.Systolic = 80;
+            bp.Diastolic = 50;
             Assert.AreEqual(bp.Category, BPCategory.Low);
         }
 
         [TestMethod]
         public void TestIdealBloodPressure()
         {
-            BloodPressure bp = new BloodPressure() { Systolic = 110, Diastolic = 70 };
+            bp.Systolic = 110;
+            bp.Diastolic = 70;
             Assert.AreEqual(bp.Category, BPCategory.Ideal);
         }
 
         [TestMethod]
         public void TestPreHighBloodPressure()
         {
-            BloodPressure bp = new BloodPressure() { Systolic = 130, Diastolic = 85 };
+            bp.Systolic = 130;
+            bp.Diastolic = 85;
             Assert.AreEqual(bp.Category, BPCategory.PreHigh);
         }
 
         [TestMethod]
         public void TestHighBloodPressure()
         {
-            BloodPressure bp = new BloodPressure() { Systolic = 170, Diastolic = 95 };
+            bp.Systolic = 170;
+            bp.Diastolic = 95;
             Assert.AreEqual(bp.Category, BPCategory.High);
         }
 
         [TestMethod]
         public void TestPreBorderLowIdealBloodPressure()
         {
-            BloodPressure bp = new BloodPressure() { Systolic = 89, Diastolic = 59 };
+            bp.Systolic = 89;
+            bp.Diastolic = 59;
             Assert.AreEqual(bp.Category, BPCategory.Low);
         }
 
         [TestMethod]
         public void TestBorderLowIdealBloodPressure()
         {
-            BloodPressure bp = new BloodPressure() { Systolic = 90, Diastolic = 60 };
+            bp.Systolic = 90;
+            bp.Diastolic = 60;
             Assert.AreEqual(bp.Category, BPCategory.Ideal);
         }
 
         [TestMethod]
         public void TestPreBorderIdealPreHighBloodPressure()
         {
-            BloodPressure bp = new BloodPressure() { Systolic = 119, Diastolic = 79 };
+            bp.Systolic = 119;
+            bp.Diastolic = 79;
             Assert.AreEqual(bp.Category, BPCategory.Ideal);
         }
 
         [TestMethod]
         public void TestBorderIdealPreHighBloodPressure()
         {
-            BloodPressure bp = new BloodPressure() { Systolic = 120, Diastolic = 80 };
+            bp.Systolic = 120;
+            bp.Diastolic = 80;
             Assert.AreEqual(bp.Category, BPCategory.PreHigh);
         }
 
         [TestMethod]
         public void TestPreBorderPreHighHighBloodPressure()
         {
-            BloodPressure bp = new BloodPressure() { Systolic = 139, Diastolic = 89 };
+            bp.Systolic = 139;
+            bp.Diastolic = 89;
             Assert.AreEqual(bp.Category, BPCategory.PreHigh);
         }
 
         [TestMethod]
         public void TestBorderPreHighHighBloodPressure()
         {
-            BloodPressure bp = new BloodPressure() { Systolic = 140, Diastolic = 90 };
+            bp.Systolic = 140;
+            bp.Diastolic = 90;
             Assert.AreEqual(bp.Category, BPCategory.High);
+        }
+
+        [TestMethod]
+        public void TestInvalidBloodPressureValues()
+        {
+            // Arrange
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => bp.Systolic = -1);
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => bp.Diastolic = -1);
         }
 
     }
